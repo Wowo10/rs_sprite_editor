72d0b90a53f2fda10b4346f725c80f0ca2c47e16
Wowo10 <wojtekwojtek529@gmail.com>
2018-12-04 22:02:37 +0100

    canvas not working.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ extern crate imgui_opengl_renderer;
 extern crate sdl2;
 extern crate sdl2imgui;
 
+use std::path::Path;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Point;
+use sdl2::rect::Rect;
+use std::time::Duration;
+
 fn main() {
 	let sdl_context = sdl2::init().unwrap();
 	let video = sdl_context.video().unwrap();
@@ -17,12 +25,47 @@ fn main() {
 	let window = video
 		.window("sdl2imgui", 800, 600)
 		.position_centered()
-		.resizable()
 		.opengl()
 		.build()
 		.unwrap();
+	// .position_centered()
+	// .resizable()
+	// .opengl()
+	// .build()
+	// .unwrap();
 
-	let _gl_context = window
+	let mut canvas = window.into_canvas()
+        .accelerated().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+
+	canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
+
+	let mut timer = sdl_context.timer().unwrap();
+
+    //let mut event_pump = sdl_context.event_pump().unwrap();
+
+	let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/characters.bmp")).unwrap();
+    let texture = texture_creator.create_texture_from_surface(&temp_surface).unwrap();
+
+    let frames_per_anim = 4;
+    let sprite_tile_size = (32,32);
+
+    // Baby - walk animation
+    let mut source_rect_0 = Rect::new(0, 0, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_0 = Rect::new(0, 0, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_0.center_on(Point::new(-64,120));
+
+    // King - walk animation
+    let mut source_rect_1 = Rect::new(0, 32, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_1 = Rect::new(0, 32, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_1.center_on(Point::new(0,240));
+
+    // Soldier - walk animation
+    let mut source_rect_2 = Rect::new(0, 64, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_2 = Rect::new(0, 64, sprite_tile_size.0*4, sprite_tile_size.0*4);
+    dest_rect_2.center_on(Point::new(440,360));
+
+	let _gl_context = &canvas.window()
 		.gl_create_context()
 		.expect("Couldn't create GL context");
 	gl::load_with(|s| video.gl_get_proc_address(s) as _);
@@ -59,7 +102,9 @@ fn main() {
 			}
 		}
 
-		let ui = imgui_sdl2.frame(&window, &mut imgui, &event_pump);
+		let ticks = timer.ticks() as i32;
+
+		let ui = imgui_sdl2.frame(&canvas.window(), &mut imgui, &event_pump);
 		ui.show_demo_window(&mut true);
 
 		unsafe {
@@ -69,8 +114,26 @@ fn main() {
 
 		renderer.render(ui);
 
-		window.gl_swap_window();
+		// window.gl_swap_window();
 
-		::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+		// ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+
+		source_rect_0.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_0.set_x(1 * ((ticks / 14) % 768) - 128);
+
+        source_rect_1.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_1.set_x((1 * ((ticks / 12) % 768) - 672) * -1);
+
+        source_rect_2.set_x(32 * ((ticks / 100) % frames_per_anim));
+        dest_rect_2.set_x(1 * ((ticks / 10) % 768) - 128);
+
+		canvas.clear();
+        // copy the frame to the canvas
+        canvas.copy_ex(&texture, Some(source_rect_0), Some(dest_rect_0), 0.0, None, false, false).unwrap();
+        canvas.copy_ex(&texture, Some(source_rect_1), Some(dest_rect_1), 0.0, None, true, false).unwrap();
+        canvas.copy_ex(&texture, Some(source_rect_2), Some(dest_rect_2), 0.0, None, false, false).unwrap();
+        canvas.present();
+
+        std::thread::sleep(Duration::from_millis(100));
 	}
 }
