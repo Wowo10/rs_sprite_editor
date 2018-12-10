cdf04a5b7436315c14ff6587975ffa61c02aa256
Wowo10 <wojtekwojtek529@gmail.com>
2018-12-10 15:23:41 +0100

    Deleted some things.

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn main() {
         Err(err) => panic!("SDL could not create a window! SDL_Error: {}.", err),
     };
 
-    let _gl_context = match window.gl_create_context() {
+    let gl_context = match window.gl_create_context() {
         Ok(gl_context) => gl_context,
         Err(err) => panic!("SDL could not create Gl Context: {}.", err),
     };
@@ -86,16 +86,6 @@ fn main() {
     let mut dest_rect_0 = Rect::new(0, 0, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
     dest_rect_0.center_on(Point::new(-64, 120));
 
-    // King - walk animation
-    let mut source_rect_1 = Rect::new(0, 32, sprite_tile_size.0, sprite_tile_size.0);
-    let mut dest_rect_1 = Rect::new(0, 32, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
-    dest_rect_1.center_on(Point::new(0, 240));
-
-    // Soldier - walk animation
-    let mut source_rect_2 = Rect::new(0, 64, sprite_tile_size.0, sprite_tile_size.0);
-    let mut dest_rect_2 = Rect::new(0, 64, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
-    dest_rect_2.center_on(Point::new(440, 360));
-
     'running: loop {
         use sdl2::event::Event;
         use sdl2::keyboard::Keycode;
@@ -120,15 +110,7 @@ fn main() {
 
         source_rect_0.set_x(32 * ((ticks / 100) % frames_per_anim));
         dest_rect_0.set_x(1 * ((ticks / 14) % 768) - 128);
-
-        source_rect_1.set_x(32 * ((ticks / 100) % frames_per_anim));
-        dest_rect_1.set_x((1 * ((ticks / 12) % 768) - 672) * -1);
-
-        source_rect_2.set_x(32 * ((ticks / 100) % frames_per_anim));
-        dest_rect_2.set_x(1 * ((ticks / 10) % 768) - 128);
-
-        //draw shit
-        canvas.window_mut().gl_make_current(&_gl_context).unwrap();
+        
         canvas.set_draw_color(sdl2::pixels::Color::RGB(20, 200, 20));
         canvas.clear();
 
@@ -142,49 +124,20 @@ fn main() {
                 false,
                 false,
             ).unwrap();
-        canvas
-            .copy_ex(
-                &texture,
-                Some(source_rect_1),
-                Some(dest_rect_1),
-                0.0,
-                None,
-                true,
-                false,
-            ).unwrap();
-        canvas
-            .copy_ex(
-                &texture,
-                Some(source_rect_2),
-                Some(dest_rect_2),
-                0.0,
-                None,
-                false,
-                false,
-            ).unwrap();
 
         //ui render
         let ui = imgui_sdl2.frame(&canvas.window(), &mut imgui, &event_pump);
         ui.show_demo_window(&mut true);
 
-        // unsafe {
-        //     gl::ClearColor(0.9, 0.2, 0.2, 1.0);
-        //     gl::Clear(gl::COLOR_BUFFER_BIT);
-        // }
-        unsafe { gl::Flush() };
-
         canvas.set_draw_color(sdl2::pixels::Color::RGB(200, 20, 20));
         canvas
             .draw_rect(sdl2::rect::Rect::new(100, 100, 200, 200))
             .unwrap();
-        unsafe { gl::Flush() };
 
-        canvas.window_mut().gl_make_current(&_gl_context).unwrap();
+        canvas.window_mut().gl_make_current(&gl_context).unwrap();
         renderer.render(ui);
-        unsafe { gl::Flush() };
 
         &canvas.present();
-        //&canvas.window().gl_swap_window();
 
         ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
