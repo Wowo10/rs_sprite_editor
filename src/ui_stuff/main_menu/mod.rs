289d8ffb77984b55f32b55fedf38db9842db3d83
Wowo10 <wojtekwojtek529@gmail.com>
2018-12-28 11:18:12 +0100

    Fixed inputs error.

--- src/ui_stuff/main_menu/mod.rs
+++ src/ui_stuff/main_menu/mod.rs
@@ -19,8 +19,18 @@ impl MainMenuInterface {
             save: false,
             add: false,
 
-            save_input: ImString::new(""),
+            save_input: ImString::with_capacity(32),
         }
+    }
+
+    pub fn reset(&mut self) {
+        self.exit = false;
+        self.open = false;
+        self.load = false;
+        self.save = false;
+        self.add = false;
+
+        self.save_input.clear();
     }
 }
 
@@ -29,12 +39,16 @@ impl UserInterface for MainMenuInterface {
         ui.main_menu_bar(|| {
             ui.menu(im_str!("File")).build(|| {
                 if ui.menu_item(im_str!("New")).build() {
+                    self.reset();
                     //Some file menu to choose basic texture
                 }
                 if ui.menu_item(im_str!("Load")).build() {
+                    self.reset();
+                    self.load = true;
                     //Some file menu to choose other config.csv
                 }
                 if ui.menu_item(im_str!("Save")).build() {
+                    self.reset();
                     self.save = true;
                 }
                 if ui.menu_item(im_str!("Exit")).build() {
@@ -49,31 +63,29 @@ impl UserInterface for MainMenuInterface {
             });
         });
 
+        if self.load {
+            ui.window(im_str!("Load File"))
+                .size((300.0, 100.0), ImGuiCond::Once)
+                .position((100.0, 100.0), ImGuiCond::Once)
+                .build(|| {
+
+                    ui.text("Here will be File Browser -> WiP");
+
+                    ui.separator();
+
+                    if ui.button(im_str!("Load!"), ImVec2::new(0.0, 0.0)) {
+                        println!("Save: {:?}", self.save_input);
+                    }
+                });
+        }
+
         if self.save {
             ui.window(im_str!("Save File"))
                 .size((300.0, 100.0), ImGuiCond::Once)
                 .position((100.0, 100.0), ImGuiCond::Once)
-                .title_bar(true)
-                .movable(true)
-                .resizable(false)
-                .save_settings(false)
-                .inputs(true) // interacting with buttons.
-                .collapsible(false)
-                .scroll_bar(false)
-                .always_auto_resize(true)
                 .build(|| {
                     ui.input_text(im_str!("Filename"), &mut self.save_input)
-                        .auto_select_all(true)
-                        .chars_noblank(true)
-                        .chars_uppercase(true)
                         .build();
-
-                    ui.input_text_multiline(
-                        im_str!("Filename2"),
-                        &mut self.save_input,
-                        ImVec2::new(0.0, 0.0),
-                    )
-                    .build();
 
                     if ui.button(im_str!("Save!"), ImVec2::new(0.0, 0.0)) {
                         println!("Save: {:?}", self.save_input);
