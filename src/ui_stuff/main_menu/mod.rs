c46512a1510b8fe569636c044746d4d3927f88a1
Wowo10 <wojtekwojtek529@gmail.com>
2018-12-30 14:18:55 +0100

    Gui Looks quite nice.

--- src/ui_stuff/main_menu/mod.rs
+++ src/ui_stuff/main_menu/mod.rs
@@ -35,7 +35,20 @@ impl MainMenuInterface {
 
     pub fn reset(&mut self) {
         self.text_input.clear();
-        self.list_directory = file_utils::get_imgui_directory("./resources/spritesheets");
+        self.selected = 0;
+
+        match self.window {
+            WindowVisible::AddDoodad => {
+                self.list_directory = file_utils::get_imgui_directory("./resources/doodads")
+            }
+            WindowVisible::ChangeSpritesheet => {
+                self.list_directory = file_utils::get_imgui_directory("./resources/spritesheets")
+            }
+            WindowVisible::Load | WindowVisible::Save => {
+                self.list_directory = file_utils::get_imgui_directory("./resources/definitions")
+            }
+            _ => {}
+        }
     }
 }
 
@@ -44,16 +57,16 @@ impl UserInterface for MainMenuInterface {
         ui.main_menu_bar(|| {
             ui.menu(im_str!("File")).build(|| {
                 if ui.menu_item(im_str!("New")).build() {
-                    self.reset();
                     self.window = WindowVisible::New;
+                    self.reset();
                 }
                 if ui.menu_item(im_str!("Load")).build() {
-                    self.reset();
                     self.window = WindowVisible::Load;
+                    self.reset();
                 }
                 if ui.menu_item(im_str!("Save")).build() {
-                    self.reset();
                     self.window = WindowVisible::Save;
+                    self.reset();
                 }
                 if ui.menu_item(im_str!("Exit")).build() {
                     self.exit = true;
@@ -62,12 +75,12 @@ impl UserInterface for MainMenuInterface {
 
             ui.menu(im_str!("Fragments")).build(|| {
                 if ui.menu_item(im_str!("Add Doodad")).build() {
-                    self.reset();
                     self.window = WindowVisible::AddDoodad;
+                    self.reset();
                 }
                 if ui.menu_item(im_str!("Change SpriteSheet")).build() {
-                    self.reset();
                     self.window = WindowVisible::ChangeSpritesheet;
+                    self.reset();
                 }
             });
         });
@@ -84,57 +97,112 @@ impl UserInterface for MainMenuInterface {
 
                         if ui.button(im_str!("Yes!!"), ImVec2::new(0.0, 0.0)) {
                             println!("New: ");
+                            self.window = WindowVisible::None;
                         }
                     });
             }
 
             WindowVisible::Load => {
                 ui.window(im_str!("Load File"))
-                    .size((300.0, 100.0), ImGuiCond::Once)
+                    .size((300.0, 400.0), ImGuiCond::Once)
                     .position((100.0, 100.0), ImGuiCond::Once)
                     .build(|| {
-                        ui.text("Here will be File Browser -> WiP");
+                        ui.child_frame(im_str!("child frame"), (280.0, 200.0))
+                            .show_borders(true)
+                            .always_show_vertical_scroll_bar(true)
+                            .build(|| {
+                                for i in 0..self.list_directory.len() {
+                                    if ui.selectable(
+                                        &self.list_directory[i],
+                                        i == self.selected,
+                                        ImGuiSelectableFlags::empty(),
+                                        ImVec2::new(0.0, 0.0),
+                                    ) {
+                                        self.selected = i;
+                                    }
+                                }
+                            });
 
                         ui.separator();
 
                         if ui.button(im_str!("Load!"), ImVec2::new(0.0, 0.0)) {
-                            println!("Load: ");
+                            if self.selected < self.list_directory.len() {
+                                println!("Load: ");
+                                self.window = WindowVisible::None;
+                            }
                         }
                     });
             }
 
             WindowVisible::Save => {
                 ui.window(im_str!("Save File"))
-                    .size((300.0, 100.0), ImGuiCond::Once)
+                    .size((300.0, 400.0), ImGuiCond::Once)
                     .position((100.0, 100.0), ImGuiCond::Once)
                     .build(|| {
+                        ui.child_frame(im_str!("child frame"), (280.0, 200.0))
+                            .show_borders(true)
+                            .always_show_vertical_scroll_bar(true)
+                            .build(|| {
+                                for i in 0..self.list_directory.len() {
+                                    if ui.selectable(
+                                        &self.list_directory[i],
+                                        false,
+                                        ImGuiSelectableFlags::empty(),
+                                        ImVec2::new(0.0, 0.0),
+                                    ){
+                                        self.text_input = self.list_directory[i].clone();
+                                    }
+                                }
+                            });
+
                         ui.input_text(im_str!("Filename"), &mut self.text_input)
                             .build();
 
                         if ui.button(im_str!("Save!"), ImVec2::new(0.0, 0.0)) {
-                            println!("Save: {:?}", self.text_input);
+                            if self.text_input != ImString::new("") {
+                                println!("Save: {:?}", self.text_input);
+                                self.window = WindowVisible::None;
+                            }
                         }
                     });
             }
 
             WindowVisible::AddDoodad => {
                 ui.window(im_str!("Doodad choose"))
-                    .size((300.0, 100.0), ImGuiCond::Once)
+                    .size((300.0, 400.0), ImGuiCond::Once)
                     .position((100.0, 100.0), ImGuiCond::Once)
                     .build(|| {
-                        ui.text("Here will be File Browser -> WiP");
+                        ui.child_frame(im_str!("child frame"), (280.0, 200.0))
+                            .show_borders(true)
+                            .always_show_vertical_scroll_bar(true)
+                            .build(|| {
+                                for i in 0..self.list_directory.len() {
+                                    if ui.selectable(
+                                        &self.list_directory[i],
+                                        i == self.selected,
+                                        ImGuiSelectableFlags::empty(),
+                                        ImVec2::new(0.0, 0.0),
+                                    ) {
+                                        self.selected = i;
+                                    }
+                                }
+                            });
 
                         ui.separator();
 
                         if ui.button(im_str!("Add!"), ImVec2::new(0.0, 0.0)) {
-                            println!("Add: ");
+                            println!(
+                                "Add to: {:?}, frames: {:?}",
+                                self.list_directory[self.selected], self.text_input
+                            );
+                            self.window = WindowVisible::None;
                         }
                     });
             }
 
             WindowVisible::ChangeSpritesheet => {
                 ui.window(im_str!("SpriteSheet choose"))
-                    .size((300.0, 500.0), ImGuiCond::Once)
+                    .size((300.0, 400.0), ImGuiCond::Once)
                     .position((100.0, 100.0), ImGuiCond::Once)
                     .build(|| {
                         ui.child_frame(im_str!("child frame"), (280.0, 200.0))
@@ -167,6 +235,7 @@ impl UserInterface for MainMenuInterface {
                                     "Change to: {:?}, frames: {:?}",
                                     self.list_directory[self.selected], self.text_input
                                 );
+                                self.window = WindowVisible::None;
                             }
                         }
                     });
